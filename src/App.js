9035da81ab2f8062ffd1aba2be4b0f59a474da52
vinuji2 <vingg80@gmail.com>
2023-09-06 12:14:24 +0530

    changed the api n mafe the required changes

--- src/App.js
+++ src/App.js
@@ -1,72 +1,68 @@
-import React from 'react';
-import {useState,useEffect } from 'react';
-import MovieCard from './MovieCard';
-
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import SearchIcon from './search.svg';
+import MovieCard from './MovieCard';
 
-const API_URL='http://www.omdbapi.com?apikey=8c70dd9f';
+const API_KEY = '20b44604104cb5298e803523525389d6';
+const API_URL = 'https://api.themoviedb.org/3/search/movie';
+const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
 
-const movie1 = {
 
-    
-        "Title": "Italian Spiderman",
-        "Year":"2007",
-        "imdbID":"tt2705436",
-        "Type": "movie",
-        "Poster": "https://m.media-amazon.com/images/M/MV5BZWQxMjcwNjItZjI0ZC00ZTc4LWIwMzItM2Q0YTZhNzI3NzdlXkEyXkFqcGdeQXVyMTA0MTM5NjI2._V1_SX300.jpg"
-    
-}
 const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
-    const [movies,setMovies] = useState([]);
-    const [searchTerm,setSearchTerm] = useState('');
-    const searchMovies = async(title) => {
-        const response = await fetch(`${API_URL}&s=${title}`);
-        const data = await response.json();
+  const searchMovies = async (query) => {
+    try {
+      const response = await fetch(`${API_URL}?api_key=${API_KEY}&query=${query}`);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
 
-        setMovies(data.Search);
+      const data = await response.json();
+      setMovies(data.results);
+    } catch (error) {
+      console.error('Error:', error);
     }
+  };
 
-    useEffect(() =>{
-        searchMovies('Spiderman');
+  useEffect(() => {
+    searchMovies('Spiderman');
+  }, []);
 
-    },[])
-    return (
-        <div className = "app">
-            <h1>MovieLand</h1>
+  return (
+    <div className="app">
+      <h1>Movieland</h1>
 
-            <div className = "search">
-                <input
-                placeholder="Search for Movies"
-                value  ={searchTerm}
-                onChange = {(e) =>setSearchTerm(e.target.value)}
-                />
-                <img
-                src = {SearchIcon}
-                alt = "search"
-                onClick = {() => searchMovies(searchTerm)}
-                />
-            </div>
-            {
-                movies?.length >0
-                ?(
-                    <div className = "container">
-                        {movies.map((movie)=>(
-                            <MovieCard movie = {movie}/>
-                        )
-                        )}
-                   </div>
+      <div className="search">
+        <input
+          placeholder="Search for Movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+         <img
+            src={SearchIcon}
+            alt="search"
+            onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
 
-                ):(
-                    <div className ="empty">
-                        <h2>No movies found</h2>
-                    </div>
-                )
-            }
-        
-        </div>
-    );
-}
+      {
+        movies?.length > 0 ? (
+          <div className="container">
+            {movies.map((movie) => (
+              <MovieCard key={movie.id} movie={movie} /> 
+            ))}
+          </div>
+        ) : (
+          <div className="empty">
+            <h2>No movies Found</h2>
+          </div>
+        )
+      }
+    </div>
+  );
+};
 
+export { IMAGE_BASE_URL };
 export default App;
