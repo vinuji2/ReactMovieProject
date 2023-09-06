14aea203423f36f7ad6b2b7260abfe4ceac1f58c
vinuji2 <vingg80@gmail.com>
2023-09-06 13:19:17 +0530

    hash routing add

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-
 import { createRoot } from 'react-dom/client';
+import { HashRouter } from 'react-router-dom'; // Import HashRouter
 import App from './App';
 
 const root = document.getElementById('root');
 const reactRoot = createRoot(root);
-reactRoot.render(<App />);
+reactRoot.render(
+  <HashRouter basename="/">
+    <App />
+  </HashRouter>
+);
